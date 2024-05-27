5e6501a1e6ba7b70967d4df54ac8aa6a209bcd09
Samuel <samuelketema01@gmail.com>
2024-05-27 23:35:25 +0300

    questions evaluated

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyApp extends StatefulWidget{
   
@@ -6,9 +7,16 @@ class MyApp extends StatefulWidget{
 }
 
 class  _appState extends State<MyApp>{
+  List <String> questions = [
+    'The capital city of Ethiopia is Addis Abeba',
+    'Ethiopia is one of the smallest country in Africa Continent',
+    'The prime minister of Ethiopia is Abiy Ahmed Ali'
+  ];
+  List<bool> answers =[true,false,true];
+  int questionNumber = 0;
   build(BuildContext context){
     return MaterialApp(
-      title: 'Question App'
+      title: 'Question App',
       home:Scaffold(
         appBar: AppBar(
           title:Text("Questions"),
@@ -16,12 +24,61 @@ class  _appState extends State<MyApp>{
           toolbarHeight: 55.9,
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Expanded(
+              flex: 5,
+              child: Text(questions[questionNumber])
+            ),
+            Expanded(
+              child:TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white
+                ),
+                onPressed: (){
+                late  bool ans;
+                  setState((){
+                     ans = answers[questionNumber];
+                  });
+                  if (ans){
+                    print("Correct Answer");
+                  }else{
+                    print('Wrong Answer');
+                  }
+                },
+                child: Text('True'),
+                ),
+              ),
+            Expanded(
+              child:TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white
+                ),
+                onPressed: (){
+                  late bool ans;
+                  setState((){
+                    ans = answers[questionNumber];
+                  });
+                  if(!ans){
+                    print('Correct Answer');
+                  }else{
+                    print('Incorrect Answer');
+                  }
+                },
+                child: Text('False'),
+                ),
+              )
+            
             
           ],
         ),
       )
     );
   }
+
+
 
 }
